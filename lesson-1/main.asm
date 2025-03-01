5957d272685cf3a4211631f78cb8252db38c6909
Bar Einav <bareinav10@gmail.com>
2025-03-01 15:59:25 +0000

    Implement lesson 1 for 64 bit machine

---
+++ lesson-1/main.asm
@@ -0,0 +1,14 @@
+BITS 64
+
+SECTION .data
+msg	db	'Hello World!', 0Ah
+
+SECTION .text
+global _start
+
+_start:
+	mov rax, 1      ; sys_write
+	mov rdi, 1	; STD_OUT
+	mov rsi, msg	; message
+	mov rdx, 13	; length
+	syscall
